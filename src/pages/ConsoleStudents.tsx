f6856ba9c431d649e03c96c7ac8a29e1784fabad
Ndeh-007 <ndehngwa@gmail.com>
2021-06-10 05:37:07 +0100

    Completed UI, failure to attach node js,

--- src/pages/ConsoleStudents.tsx
+++ src/pages/ConsoleStudents.tsx
@@ -1,12 +1,211 @@
-import { IonPage } from '@ionic/react'
-import React from 'react'
+import {
+  IonAvatar,
+  IonBackdrop,
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardTitle,
+  IonCol,
+  IonContent,
+  IonFooter,
+  IonGrid,
+  IonHeader,
+  IonIcon,
+  IonImg,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonModal,
+  IonPage,
+  IonProgressBar,
+  IonRow,
+  IonSelect,
+  IonSelectOption,
+  IonText,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import React, { useRef, useState } from "react";
+import {
+  add,
+  addCircle,
+  arrowBack,
+  arrowBackCircle,
+  cloudUpload,
+  document,
+  play,
+  reader,
+} from "ionicons/icons";
+import img from "../images/img.png";
+import pdfLogo from "../images/pdfLogo.svg";
 
-const ConsoleStudents:React.FC=()=>{
-    return(
-        <IonPage>
+import "../css/console.css";
 
-        </IonPage>
-    );
+const ConsoleStudents: React.FC = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [reviewMessage, setreviewMessage] = useState(
+    "Writren review will appear..."
+  );
+  const review = useRef<HTMLIonInputElement>(null);
+  const upload = useRef<HTMLInputElement>(null);
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar className="tb-console">
+          <IonIcon
+            color="success"
+            slot="start"
+            size="large"
+            icon={arrowBackCircle}
+          ></IonIcon>
+          <IonText slot="start" color="success">
+            LogOut <IonText color="dark">/ Projects</IonText>
+          </IonText>
+          <IonTitle slot="end">Oben Desmond</IonTitle>
+          <IonAvatar slot="end" className="tb-avatar">
+            <IonImg src={img}></IonImg>
+          </IonAvatar>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonGrid>
+          <IonRow>
+            <IonCol sizeMd="3" className="db-col">
+              <div className="holder">
+                <div style={{ width: "90%", margin: "10px auto" }}>
+                  <div className="lp-titles">
+                    <IonText color="success">
+                      <h1>
+                        Projects for{" "}
+                        <IonText color="dark">2nd Semester</IonText> 2021
+                      </h1>
+                    </IonText>
+                    <IonButton
+                      size="large"
+                      fill="solid"
+                      color="primary"
+                      expand="block"
+                      style={{ "margin-top": "30px" }}
+                      onClick={() => {
+                        upload.current?.click();
+                      }}
+                    >
+                      <IonIcon slot="start" icon={cloudUpload}></IonIcon>
+                      upload paper
+                    </IonButton>
+                  </div>
+                  <input
+                    type="file"
+                    ref={upload}
+                    name="file"
+                    id="uploadFile"
+                    style={{ display: "none" }}
+                  />
+                </div>
+                <div className="item-holder">
+                  <IonItem lines="inset">
+                    <IonImg slot="start" src={pdfLogo}></IonImg>
+                    {/* <IonAvatar slot="start">
+                    </IonAvatar> */}
+                    <div className="lh">
+                      <IonText>
+                        <h5>LOTOS Verificaton of Bubble Sort</h5>
+                      </IonText>
+                    </div>
+                  </IonItem>
+                  <IonItem lines="inset">
+                    <IonImg slot="start" src={pdfLogo}></IonImg>
+                    {/* <IonAvatar slot="start">
+                    </IonAvatar> */}
+                    <div className="lh">
+                      <IonText>
+                        <h5>LOTOS Verificaton of Bubble Sort</h5>
+                      </IonText>
+                    </div>
+                  </IonItem>
+                  <IonItem lines="inset">
+                    <IonImg slot="start" src={pdfLogo}></IonImg>
+                    {/* <IonAvatar slot="start">
+                    </IonAvatar> */}
+                    <div className="lh">
+                      <IonText>
+                        <h5>LOTOS Verificaton of Bubble Sort</h5>
+                      </IonText>
+                    </div>
+                  </IonItem>
+                </div>
+              </div>
+            </IonCol>
+            <IonCol sizeMd="9">
+              <div className="holder">
+                <IonItem lines="full">
+                  <div slot="start">
+                    <h1>Software Verification and Validation</h1>
+                    <h6>
+                      <i>Date Submitted</i>
+                    </h6>
+                  </div>
+                  <div slot="end">
+                    {/* <IonButton style={{'display':'none'}} className="sr-button">send review</IonButton> */}
+                    <h6>12 JUNE 2021</h6>
+                  </div>
+                </IonItem>
+                <IonItem lines="full">
+                  <div slot="start">
+                    <h1>Tutor</h1>
+                  </div>
+                  <div slot="end">
+                    <h1>Mme Tsague</h1>
+                  </div>
+                </IonItem>
+                <IonItem lines="full">
+                  <div slot="start">
+                    <h1>Status</h1>
+                  </div>
+                  <div slot="end">
+                    <h1>
+                      <IonText color="danger">rejected</IonText>
+                    </h1>
+                  </div>
+                </IonItem>
+                <div className="rm-area">
+                  <IonCard color="light">
+                    <IonCardHeader>
+                      <IonCardTitle>
+                        <b>
+                          <IonText color="success"> Review Message</IonText>
+                        </b>
+                      </IonCardTitle>
+                    </IonCardHeader>
+                    <IonCardContent>
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                      Sint nisi est placeat qui maiores. Amet eos qui, placeat
+                      officia nobis dolorum dolor perferendis ullam magnam sed
+                      quis, aut nemo veritatis.
+                    </IonCardContent>
+                  </IonCard>
+
+                  <div style={{'width':'40%', margin: "30px auto" }}>
+                    <IonButton
+                      size="large"
+                      fill="solid"
+                      color="primary"
+                      expand="block" 
+                    > 
+                      <IonIcon slot="start" icon={reader}></IonIcon> Open
+                      Document
+                    </IonButton>
+                  </div>
+                </div>
+              </div>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+    </IonPage>
+  );
 };
 
-export default ConsoleStudents
+export default ConsoleStudents;
--- src/pages/ConsoleTeachers.tsx
+++ src/pages/ConsoleTeachers.tsx
@@ -1,13 +1,14 @@
 import {
   IonAvatar,
+  IonBackdrop,
   IonButton,
-  IonButtons,
   IonCard,
   IonCardContent,
   IonCardHeader,
   IonCardTitle,
   IonCol,
   IonContent,
+  IonFooter,
   IonGrid,
   IonHeader,
   IonIcon,
@@ -15,7 +16,9 @@ import {
   IonInput,
   IonItem,
   IonLabel,
+  IonModal,
   IonPage,
+  IonProgressBar,
   IonRow,
   IonSelect,
   IonSelectOption,
@@ -23,13 +26,50 @@ import {
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { play } from "ionicons/icons";
 import img from "../images/img.png";
 
 import "../css/console.css";
 
 const ConsoleTeachers: React.FC = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [reviewMessage, setreviewMessage] = useState(
+    "Writren review will appear..."
+  );
+  const review = useRef<HTMLIonInputElement>(null);
+
+  let axios = require("axios");
+
+  function getValues() {
+    // const options = {
+    //   method: "POST",
+    //   origin: "http://localhost:8100/console-teachers",
+    //   mode: "no-cors",
+    // };
+    // fetch("https://webhook.site/01ba19c2-36e0-4cb6-bd18-a8bb78cec13f", options)
+    //   .then((response) => response.json())
+    //   .then((response) => {
+    //     console.log(response.data);
+    //   });
+
+    // axios
+    //   .get(
+    //     "https://webhook.site/01ba19c2-36e0-4cb6-bd18-a8bb78cec13f",{mode:'no-cors'}
+    //   )
+    //   .then((response:any) => {
+    //     console.log(response.data);
+    //     console.log(response.status);
+    //     console.log(response.statusText);
+    //     console.log(response.headers);
+    //     console.log(response.config);
+    //   });
+  }
+
+  useEffect(() => {
+    getValues();
+  }, []);
+
   return (
     <IonPage>
       <IonHeader>
@@ -133,10 +173,59 @@ const ConsoleTeachers: React.FC = () => {
                       Sint nisi est placeat qui maiores. Amet eos qui, placeat
                       officia nobis dolorum dolor perferendis ullam magnam sed
                       quis, aut nemo veritatis.
-                      <h6>
-                        <IonButton className="b">edit</IonButton>
-                      </h6>
                     </IonCardContent>
+                    <IonItem className="i" lines="none">
+                      <IonButton
+                        className="b"
+                        slot="end"
+                        onClick={() => setShowModal(true)}
+                      >
+                        edit
+                      </IonButton>
+                    </IonItem>
+                    <IonModal
+                      onDidDismiss={() => setShowModal(false)}
+                      isOpen={showModal}
+                    >
+                      <IonContent>
+                        <IonHeader>
+                          <IonToolbar>
+                            <IonTitle>Compose Review Message</IonTitle>
+                          </IonToolbar>
+                        </IonHeader>
+                        <IonItem>
+                          <IonLabel position="floating">Enter Review</IonLabel>
+                          <IonInput
+                            ref={review}
+                            type="text"
+                            value={``}
+                            onKeyPress={() =>
+                              console.log(review.current?.value)
+                            }
+                          ></IonInput>
+                        </IonItem>
+                        <IonCard
+                          style={{ "max-height": "75%", "min-height": "60%" }}
+                        >
+                          <IonCardHeader>Message</IonCardHeader>
+                          <IonCardContent id="text">
+                            {reviewMessage}
+                          </IonCardContent>
+                        </IonCard>
+                        <IonFooter>
+                          <IonToolbar>
+                            <IonButton
+                              onClick={() => setShowModal(false)}
+                              slot="end"
+                              color="danger"
+                            >
+                              close
+                            </IonButton>
+                            <IonButton slot="end">Confirm</IonButton>
+                          </IonToolbar>
+                        </IonFooter>
+                      </IonContent>
+                    </IonModal>
                   </IonCard>
                 </div>
                 <div className="status">
@@ -145,12 +234,47 @@ const ConsoleTeachers: React.FC = () => {
                 </div>
 
                 <div className="results-holder">
-                  <div className="results-item">
-                    <IonToolbar className='results-item'> 
-                        <IonTitle>Tabi FE18A008</IonTitle>
-                        <IonTitle slot='end'>20 Copies</IonTitle> 
-                      <IonButton slot='end' fill='outline' color='success'>view</IonButton>
+                  <div className="result-item">
+                    <IonToolbar className="results-item">
+                      <IonTitle>Tabi FE18A008</IonTitle>
+                      <IonTitle slot="end">20 Copies</IonTitle>
+                      <IonButton slot="end" fill="outline" color="success">
+                        view
+                      </IonButton>
                     </IonToolbar>
+                    <IonProgressBar
+                      className="pg-bar"
+                      color="primary"
+                      value={0.5}
+                    ></IonProgressBar>
+                  </div>
+                  <div className="result-item">
+                    <IonToolbar className="results-item">
+                      <IonTitle>Tabi FE18A008</IonTitle>
+                      <IonTitle slot="end">20 Copies</IonTitle>
+                      <IonButton slot="end" fill="outline" color="success">
+                        view
+                      </IonButton>
+                    </IonToolbar>
+                    <IonProgressBar
+                      className="pg-bar"
+                      color="primary"
+                      value={0.5}
+                    ></IonProgressBar>
+                  </div>
+                  <div className="result-item">
+                    <IonToolbar className="results-item">
+                      <IonTitle>Tabi FE18A008</IonTitle>
+                      <IonTitle slot="end">20 Copies</IonTitle>
+                      <IonButton slot="end" fill="outline" color="success">
+                        view
+                      </IonButton>
+                    </IonToolbar>
+                    <IonProgressBar
+                      className="pg-bar"
+                      color="primary"
+                      value={0.5}
+                    ></IonProgressBar>
                   </div>
                 </div>
               </div>
